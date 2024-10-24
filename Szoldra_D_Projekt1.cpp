d2ca6de263892645bc33f69357e2550dd2f5f794
dasho0 <fallentomato00@gmail.com>
2024-10-24 18:44:55 +0200

    Dodaj pliki projektów.

---
+++ Szoldra_D_Projekt1.cpp
@@ -0,0 +1,9 @@
+﻿// Szoldra_D_Projekt1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
